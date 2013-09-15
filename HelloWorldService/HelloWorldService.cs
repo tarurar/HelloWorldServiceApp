0bef6f62857496c59d672fd9c2068c89ae2fb443
atarutin <tarutin_andrew@mail.ru>
2013-09-15 16:35:52 +0400

    from scrath

---
+++ HelloWorldService/HelloWorldService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldService
+{
+    public class HelloWorldService: IHelloWorldService
+    {
+        private static String GetMessage(String name)
+        {
+            return "Hello World from " + name + "!";
+        }
+
+        public string GetJsonData(string name)
+        {
+            return GetMessage(name);
+        }
+
+        public string GetXmlData(string name)
+        {
+            return GetMessage(name);
+        }
+    }
+}
---
+++ HelloWorldService/IHelloWorldService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace HelloWorldService
+{
+    [ServiceContract]
+    public interface IHelloWorldService
+    {
+        [OperationContract]
+        [WebInvoke(
+            Method = "GET", 
+            ResponseFormat = WebMessageFormat.Json, 
+            BodyStyle = WebMessageBodyStyle.Wrapped, 
+            UriTemplate = "json/getHello/{name}")]
+        String GetJsonData(String name);
+
+        [OperationContract]
+        [WebInvoke(
+            Method = "GET", 
+            ResponseFormat = WebMessageFormat.Xml, 
+            BodyStyle = WebMessageBodyStyle.Wrapped, 
+            UriTemplate = "xml/getHello/{name}")]
+        String GetXmlData(String name);
+
+    }
+}
